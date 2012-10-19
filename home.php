6d4afc961a68f5c5ace0b5afd8009cce7e0a9f5b
Agustin Amenabar <agustin@fliin.com>
2012-10-19 18:10:28 -0300

    fix g analitycs

--- home.php
+++ home.php
@@ -73,10 +73,18 @@ header('Pragma: no-cache'); ?><!doctype html>
 				<script type="text/javascript">try{Typekit.load();}catch(e){}</script><!---->
 			<script src="js/script.js"></script>
 			<script>
-	var _gaq=[['_setAccount','UA-23469330-1'],['_trackPageview']];
-	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-	s.parentNode.insertBefore(g,s)}(document,'script'));
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-23469330-1']);
+  _gaq.push(['_setDomainName', 'medula.cl']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 </script>
 
 </body>
